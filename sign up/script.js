4e32db0655a747844a6ef90db10f6663226ce62f
charles <cking6951@gmail.com>
2022-06-25 21:48:29 +0300

    signup page in progress

--- sign up/script.js
+++ sign up/script.js
@@ -1,8 +1,14 @@
 const form = document.querySelector("form");
+nField = form.querySelector(".fullname"),
+nInput = nField.querySelector('input'),
+phoneField = form.querySelector(".phone-no"),
+phoneInput = phoneField.querySelector("input"),
 eField = form.querySelector(".email"),
 eInput = eField.querySelector("input"),
 pField = form.querySelector(".password"),
-pInput = pField.querySelector("input");
+pInput = pField.querySelector("input"),
+confPassField = form.querySelector(".confirm-password"),
+confPassInput = confPassField.querySelector("input");
 
 form.onsubmit = (e)=>{
   e.preventDefault(); //preventing from form submitting
@@ -13,10 +19,12 @@ form.onsubmit = (e)=>{
   setTimeout(()=>{ //remove shake class after 500ms
     eField.classList.remove("shake");
     pField.classList.remove("shake");
+    confPassField.classList.remove("shake");
   }, 500);
 
   eInput.onkeyup = ()=>{checkEmail();} //calling checkEmail function on email input keyup
   pInput.onkeyup = ()=>{checkPass();} //calling checkPassword function on pass input keyup
+  confPassInput.onkeyup = ()=>{passMatch();} //calling matchPassword function on input keyup 
 
   function checkEmail(){ //checkEmail function
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/; //pattern for validate email
@@ -39,12 +47,23 @@ form.onsubmit = (e)=>{
     }else{ //if pass is empty then remove error and add valid class
       pField.classList.remove("error");
       pField.classList.add("valid");
+      passMatch();
+    }
+  }
+
+  function passMatch() {
+    if (pInput.value === confPassInput) {
+      confPassField.classList.add("valid");
+      confPassField.classList.remove("error");
+    } else {
+      confPassField.classList.remove("valid");
+      confPassField.classList.add("error");
     }
   }
 
   //if eField and pField doesn't contains error class that mean user filled details properly
   if(!eField.classList.contains("error") && !pField.classList.contains("error")){
-    console.log('email=', eInput.value, 'pass =', pInput.value );
+    console.log('email=', eInput.value, 'pass =', pInput.value, 'confirmed Password=', confPassInput.value );
     window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
   }
 }
