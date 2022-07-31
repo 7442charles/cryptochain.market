134962f937251c4f844d05e519c649fa4fc5d78d
charles <cking6951@gmail.com>
2022-07-31 18:29:02 +0300

    readme mod

--- forgot password/script.js
+++ forgot password/script.js
@@ -35,69 +35,86 @@ form.onsubmit = (e)=>{
   //if eField and pField doesn't contains error class that mean user filled details properly
   if(!eField.classList.contains("error")){
     //console.log(/*'email=', eInput.value,*/ 'pass =', pInput.value, "phoneNumber =", phoneInput.value);
-    fetch("http://api.cryptochainmarket.tk:8000/api/v1/auth/forget", { //the login api url
-      method: "GET",
-      body:JSON.stringify({
-        email:eInput.value,
-      }),
-    })
-    .then((response) => response.json())
-    .then((result) => {
-      if (result.status == "success") {//to display the toast of login success
-        Toastify({
-          text: "Check Your email and enter the code here  \n",
-          duration: 5000,
-          destination: "../login/loginmain.html",
-          avatar: "../imgs/IMG-20220520-WA0007-removebg-preview.png",
-          newWindow: false,
-          close: true,
-          gravity: "bottom", // `top` or `bottom`
-          position: "center", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          style: {
-            background: "linear-gradient(to right, #4C4F50, #B0B0BO)",
-          },
-          onClick: function(){} // Callback after click
-        }).showToast();
-        codeDiv.innerHTML = reset_code_div,
-        resetPass.innerHTML = reset_pass_div;
-        actionBtn.value = "reset Password";
-        document.querySelector(".code-class").focus();
-      } else {//display toast if login wasn't succefull
-        Toastify({
-          text: "Error occured! \n Check your email and try again \n",
-          duration: 8000,
-          avatar: "../imgs/IMG-20220520-WA0007-removebg-preview.png",
-          destination: "#",
-          newWindow: false,
-          close: true,
-          gravity: "top", // `top` or `bottom`
-          position: "center", // `left`, `center` or `right`
-          stopOnFocus: true, // Prevents dismissing of toast on hover
-          style: {
-            background: "linear-gradient(to right, #4C4F50, #B0B0BO)",
-          },
-          onClick: function(){} // Callback after click
-        }).showToast();
-      }
+    let email = {email:eInput.value};
+    
+    let query = Object.keys(email)
+                 .map(k => encodeURIComponent(email[k]))
+                 .join('&');
+    
+    let url = 'http://api.cryptochainmarket.tk:8000/api/v1/auth/forget/' + query;
+    
+    fetch(url)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.status == "success") {//to display the toast of login success
+          Toastify({
+            text: "Check Your email and enter the code here  \n",
+            duration: 5000,
+            destination: "../login/loginmain.html",
+            avatar: "../imgs/IMG-20220520-WA0007-removebg-preview.png",
+            newWindow: false,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #4C4F50, #B0B0BO)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+          codeDiv.innerHTML = reset_code_div,
+          resetPass.innerHTML = new_pass_div;
+          actionBtn.value = "reset Password";
+          document.querySelector(".code-class").focus();
+        } else {//display toast if login wasn't succefull
+          Toastify({
+            text: "Error occured! \n Check your email and try again \n",
+            duration: 8000,
+            avatar: "../imgs/IMG-20220520-WA0007-removebg-preview.png",
+            destination: "#",
+            newWindow: false,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #4C4F50, #B0B0BO)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+        }
     });
+
     //window.location.href = form.getAttribute("action"); //redirecting user to the specified url which is inside action attribute of form tag
   }
+  const codefield = document.querySelector(".sent-code"),
+  codeInput = document.querySelector(".code-input"),
+  newPassf = document.querySelector(".newPass"),
+  newPassI = document.querySelector(".new-password");
+
+  fetch("http://api.cryptochainmarket.tk:8000/api/v1/auth/forget", { //the login api url
+      method: "POST",
+      body:JSON.stringify({
+        email:eInput.value,
+        code:codeInput.value,
+        password:newPassI.value
+      }),
+    })
 }
 let reset_code_div = 
 `<div class="field sent-code">
     <div class="input-area">
-      <input type="text" class="code-class" placeholder="Enter code">
+      <input type="text" class="code-input" placeholder="Enter code">
       <i class="icon fas fa-user"></i>
       <i class="error error-icon fas fa-exclamation-circle"></i>
     </div>
     <div class="error error-txt">Wrong code!</div>
   </div>`;
 
-let reset_pass_div = 
-`<div class="field sent-code">
+let new_pass_div = 
+`<div class="field newPass">
     <div class="input-area">
-      <input type="password" placeholder="Enter new password">
+      <input type="password" class="new-password" placeholder="Enter new password">
       <i class="icon fas fa-lock"></i>
       <i class="error error-icon fas fa-exclamation-circle"></i>
     </div>
